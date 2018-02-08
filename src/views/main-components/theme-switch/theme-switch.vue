301515c66c631e8aa507008a243ec129c9d425b9
安森立 <ansenli@ansenlideMacBook-Pro.local>
2018-02-08 15:47:40 +0800

    优化定制主题样式

--- src/views/main-components/theme-switch/theme-switch.vue
+++ src/views/main-components/theme-switch/theme-switch.vue
@@ -36,26 +36,6 @@ export default {
                     element: '#00a854'
                 },
                 {
-                    name: 'black_y',
-                    menu: '#495060',
-                    element: '#e96500'
-                },
-                {
-                    name: 'black_r',
-                    menu: '#495060',
-                    element: '#e43e31'
-                },
-                {
-                    name: 'light_b',
-                    menu: '#495060',
-                    element: '#2d8cf0'
-                },
-                {
-                    name: 'light_g',
-                    menu: '#495060',
-                    element: '#00a854'
-                },
-                {
                     name: 'light_y',
                     menu: '#495060',
                     element: '#e96500'
